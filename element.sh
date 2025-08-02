f8c16e98c9ec71f249fb974c79a23f7913c479b2
TavishMassy <shaunmassy@gmail.com>
2025-08-02 07:55:35 -0400

    feat: added while loop to fetch all the info from db

--- element.sh
+++ element.sh
@@ -5,4 +5,9 @@ then
   echo -e "Please provide an element as an argument."
 else
   ELEMENT=$($PSQL "SELECT * FROM elements WHERE symbol='$1'")
+  echo "$ELEMENT" | while read ATOMIC_NO BAR SYMBOL BAR NAME
+  do
+    PROPERTY=$($PSQL "SELECT * FROM properties WHERE atomic_number='$ATOMIC_NO'")
+    
+  done
 fi
