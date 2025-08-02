1b40048a3687d6613fa70efd889d839fd4d4ecf9
TavishMassy <shaunmassy@gmail.com>
2025-08-02 07:46:37 -0400

    feat: added if-else statement to the element.sh

--- element.sh
+++ element.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
-echo -e "Please provide an element as an argument."
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+if [[ -z $1 ]]
+then
+  echo -e "Please provide an element as an argument."
+else
+  ELEMENT=$($PSQL "SELECT * FROM elements WHERE symbol='$1'")
+fi
