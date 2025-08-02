6d42f864b86a4145f42e4b4be5ff1d4f258ebbca
TavishMassy <shaunmassy@gmail.com>
2025-08-02 08:05:53 -0400

    feat: added ability to fetch data from types table

--- element.sh
+++ element.sh
@@ -10,6 +10,7 @@ else
     PROPERTIES=$($PSQL "SELECT * FROM properties WHERE atomic_number='$ATOMIC_NO'")
     echo "$PROPERTIES" | while read ATOMIC_NO BAR TYPE_ID BAR ATOMIC_MASS BAR MELT_POINT_CELSIUS BAR BOIL_POINT_CELSIUS
     do
+      TYPE=$($PSQL "SELECT type FROM types WHERE type_id=$TYPE_ID")
       echo -e "\nThe element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
     done
   done
