7cf0aa0ff491ffa1cd5afab4013dfba14098b4f1
TavishMassy <shaunmassy@gmail.com>
2025-08-02 08:12:38 -0400

    fix: added missing dbname in the PSQL variable

--- element.sh
+++ element.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [[ -z $1 ]]
 then
   echo -e "Please provide an element as an argument."
