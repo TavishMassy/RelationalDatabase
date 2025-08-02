0ec8284f6eb03cfc5dc1c1df5d5970096bd7f68b
TavishMassy <shaunmassy@gmail.com>
2025-08-02 08:02:40 -0400

    feat: added another while loop to fetch data from properties

--- element.sh
+++ element.sh
@@ -7,7 +7,10 @@ else
   ELEMENT=$($PSQL "SELECT * FROM elements WHERE symbol='$1' OR name='$1'")
   echo "$ELEMENT" | while read ATOMIC_NO BAR SYMBOL BAR NAME
   do
-    PROPERTY=$($PSQL "SELECT * FROM properties WHERE atomic_number='$ATOMIC_NO'")
-    echo ""
+    PROPERTIES=$($PSQL "SELECT * FROM properties WHERE atomic_number='$ATOMIC_NO'")
+    echo "$PROPERTIES" | while read ATOMIC_NO BAR TYPE_ID BAR ATOMIC_MASS BAR MELT_POINT_CELSIUS BAR BOIL_POINT_CELSIUS
+    do
+      echo -e "\nThe element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+    done
   done
 fi
