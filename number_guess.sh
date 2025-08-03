477c6be50db29a40ce71e41921084da5bbc9f124
TavishMassy <shaunmassy@gmail.com>
2025-08-03 10:41:52 -0400

    feat: added USER_HISTORY function to ask for username

--- number_guess.sh
+++ number_guess.sh
@@ -2,3 +2,15 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 RANDOM_NUM=$((1 + RANDOM % 100))
+
+USER_HISTORY() {
+  echo "Enter your username:"
+  read USERNAME
+  
+}
+
+MAIN() {
+  USER_HISTORY
+}
+
+MAIN
