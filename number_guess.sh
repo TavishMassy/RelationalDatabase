dfc157c369889d7749e23c733fc09b56c5f10d07
TavishMassy <shaunmassy@gmail.com>
2025-08-03 13:17:02 -0400

    fix: changed SQL variable in SAVE_GAME_INFO function

--- number_guess.sh
+++ number_guess.sh
@@ -48,9 +48,9 @@ GAME_MAIN() {
 SAVE_GAME_INFO() {
   let GAMES_PLAYED+=1
   BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE username='$USERNAME'")
-  if [[ $1 < $BEST_GAME ]]
+  if [[ $GUESSES < $BEST_GAME ]]
   then
-    INSERT_IN_USERS=$($PSQL "INSERT INTO users(games_played, best_game) VALUES($GAMES_PLAYED, $1) WHERE username='$USERNAME'")
+    INSERT_IN_USERS=$($PSQL "INSERT INTO users(games_played, best_game) VALUES($GAMES_PLAYED, $GUESSES) WHERE username='$USERNAME'")
   else
     INSERT_IN_USERS=$($PSQL "INSERT INTO users(games_played) VALUES($GAMES_PLAYED) WHERE username='$USERNAME'")
   fi
