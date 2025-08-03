6852666c12a957598c4a321894f7e488df69a51c
TavishMassy <shaunmassy@gmail.com>
2025-08-03 12:40:12 -0400

    feat: added function for main game

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
-
+GUESSES=0
 RANDOM_NUM=$((1 + RANDOM % 100))
 
 USER_HISTORY() {
@@ -19,7 +19,27 @@ USER_HISTORY() {
   fi
 }
 
-
+GAME_MAIN() {
+  echo "Guess the secret number between 1 and 1000:"
+  read GUESS_NUM
+  if [[ $GUESS_NUM =~ ^[0-9]+$ ]]
+  then
+    if [[ $GUESS_NUM < $RANDOM_NUM ]]
+    then
+      $GUESSES += 1
+      echo "It's lower than that, guess again:"
+    elif [[ $GUESS_NUM > $RANDOM_NUM ]]
+    then
+      $GUESSES += 1
+      echo "It's higher than that, guess again:"
+    else
+      $GUESSES += 1
+      You guessed it in $GUESSES tries. The secret number was $RANDOM_NUM.
+    fi
+  else
+    echo "That is not an integer, guess again:"
+  fi
+}
 
 MAIN() {
   USER_HISTORY
